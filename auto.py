45e6f5724528794254d06160008420a8b69837d6
Alex King <Alex@Alexs-MacBook.local>
2015-05-25 14:29:50 -0700

    Updated autoplay script

--- auto.py
+++ auto.py
@@ -1,5 +1,19 @@
+# This is an automatic player for Solitaire. It will keep track of wins and
+# losses. Use CTRL + C to quit and see the tally.
+
 from Solitaire import Solitaire
 
+wins = 0
+losses = 0
+
 while True:
-    sol = Solitaire()
-    if sol.auto_run() == True: break
+    try:
+        sol = Solitaire()
+        if sol.auto_run() == True:
+            wins += 1
+        else:
+            losses += 1
+    except KeyboardInterrupt:
+        print "\nWins: " + str(wins)
+        print "Losses: " + str(losses)
+        exit(1)
